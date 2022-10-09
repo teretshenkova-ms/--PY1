c99295944ab131355141ae9c6e008d41afae062d
teretshenkova-ms <115409457+teretshenkova-ms@users.noreply.github.com>
2022-10-09 17:08:41 +0300

    Лабораторная_работа_0

---
+++ Лабораторная_работа_0/task_8.py
@@ -0,0 +1,7 @@
+varname = "test"  # TODO переименовать название переменной в соответствии с PEP8
+
+CONST_VAR = "old_value"
+print(CONST_VAR)
+
+ANOTHER_CONST_VAR = "new_value"  # TODO убрать перезапись константы
+print(ANOTHER_CONST_VAR)
